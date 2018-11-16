65fc076754d107bbf045ebad4ef8f6faa73e42ac
IanjhPhillips <ianjhphillips@gmail.com>
2018-11-16 14:17:51 -0500

    First Commit
    First Stable version of game includes:
    
    -Infinite respawning of obstacles
    -Running animation
    -Variable jumping
    -Variable obstacles

---
+++ Sticky Runner/Assets/Scripts/Cactus.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class Cactus : MonoBehaviour {
+
+    private const int spriteCount = 3;
+
+    private static Cactus lastCactus;
+
+    private PolygonCollider2D poly;
+
+    private SpriteRenderer spriteRender;
+
+    private static float spawnDistance = 24f;
+    private static Vector3 spawn = new Vector3(spawnDistance, -4.5f, 0f);
+    private static float spawnRandomMax = 4f;
+
+    private static float endX = -16f;
+    private static float speed = 15f;
+    private static float acceleration = 0.001f;
+
+    private static Sprite[] cactiSprites;
+
+    private void Awake()
+    {
+        poly = this.GetComponent<PolygonCollider2D>();
+        if (lastCactus == null)
+        {
+            lastCactus = this;
+            this.gameObject.transform.position = spawn;
+            cactiSprites = new Sprite[spriteCount];
+            for (int i = 0; i < spriteCount; i++)
+            {
+
+                // cactiSprites[i] = (Sprite)AssetDatabase.LoadAssetAtPath("Assets/Sprites/Obstacles/cactus" + i, typeof(Sprite));
+                cactiSprites[i] = (Sprite) Resources.Load<Sprite>("Sprites/Obstacles/cactus" + i);
+                print(cactiSprites[i]);
+
+            }
+        }
+        else
+            Respawn();
+    }
+
+
+    // Use this for initialization
+    void Start () {
+
+        
+
+        speed = 15f;
+        endX = -16f;
+        spawnDistance = 24f;
+        spawnRandomMax = 4f;
+
+        spriteRender = this.GetComponent<SpriteRenderer>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        //spawnDistance += acceleration;
+        spawn = new Vector3(spawnDistance, -4.5f, 0f);
+        //spawnRandomMax -= acceleration;
+
+        speed += acceleration;
+        gameObject.transform.position -= new Vector3(speed * Time.deltaTime, 0f, 0f);
+        if (gameObject.transform.position.x <= endX)
+        {
+            Respawn((int)Random.Range(0,3));
+        }
+
+    }
+
+    private void Respawn()
+    {
+
+
+        //spriteRender.sprite = cactiSprites[index];
+        Destroy(poly);
+        poly = this.gameObject.AddComponent<PolygonCollider2D>();
+        poly.isTrigger = true;
+        this.transform.position = spawn + new Vector3(lastCactus.gameObject.transform.position.x + Random.Range(-spawnRandomMax, spawnRandomMax), 0f, 0f);
+        lastCactus = this;
+
+    }
+
+    private void Respawn(int index) {
+
+        if (!(cactiSprites.Length <= index || cactiSprites[index] == null))
+            spriteRender.sprite = cactiSprites[index];
+        else print("bad index");
+
+        Destroy(poly);
+        poly = this.gameObject.AddComponent<PolygonCollider2D>();
+        poly.isTrigger = true;
+        this.transform.position = spawn + new Vector3(lastCactus.gameObject.transform.position.x + Random.Range(-spawnRandomMax,spawnRandomMax), 0f, 0f);
+        lastCactus = this;
+
+    }
+}
---
+++ Sticky Runner/Assets/Scripts/Runner.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Runner : MonoBehaviour {
+
+    private Animator spriteAnimator;
+    private Rigidbody2D rigidBody;
+
+    private bool grounded;
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+
+        // print("colliding");
+        // print(collider.transform.tag);
+        if (collider.transform.tag == "Cactus")
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            print("You died!");
+        }
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+
+        // print("colliding");
+        // print(collider.transform.tag);
+        if (collision.transform.tag == "Jumpable")
+        {
+            grounded = true;
+        }
+
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+
+        // print("colliding");
+        // print(collider.transform.tag);
+        if (collision.transform.tag == "Jumpable")
+        {
+            grounded = false;
+        }
+
+    }
+
+    // Use this for initialization
+    void Start () {
+
+        grounded = true;
+        spriteAnimator = this.gameObject.GetComponent<Animator>();
+        rigidBody = this.GetComponent<Rigidbody2D>(); 
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        /*
+        if (rigidBody.velocity.y > 0)
+            rigidBody.gravityScale = 1f;
+        
+        else
+            rigidBody.gravityScale = 10f;
+        */
+        if (Input.GetKeyDown("space") && grounded ) {
+
+            
+            rigidBody.gravityScale = 2f;
+            rigidBody.AddForce(new Vector2(0f, 15f), ForceMode2D.Impulse);
+            
+        }
+        else if (Input.GetKeyUp("space") || rigidBody.velocity.y < 0)
+        {
+            print("release");
+            rigidBody.gravityScale = 5f;   
+
+        }
+
+
+    }
+}
